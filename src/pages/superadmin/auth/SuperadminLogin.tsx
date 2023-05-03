21705352cf45e33a4687264cb646c8cfed8ae7f1
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-05-04 00:10:47 +0100

    superadmin login page

---
+++ src/pages/superadmin/auth/SuperadminLogin.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuperadminLogin = () => {
+  return (
+    <React.Fragment>
+      <h1>Login</h1>
+    </React.Fragment>
+  )
+}
+
+export default SuperadminLogin
