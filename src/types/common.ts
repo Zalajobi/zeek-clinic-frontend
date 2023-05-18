82411d77bcd3a81bbd234e240a0ba91df03428cc
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-05-18 10:34:46 +0100

    upload media to s3bucket

---
+++ src/types/common.ts
@@ -0,0 +1,6 @@
+export type S3UploadResponse = {
+  Location: string,
+  key: string,
+  Key: string,
+  Bucket: string
+}
---
+++ src/util/useS3Upload.ts
@@ -0,0 +1,29 @@
+import * as AWS from "aws-sdk";
+import {S3UploadResponse} from "../types/common";
+
+const bucketName = process.env.REACT_APP_S3_BUCKET_NAME;
+const region = process.env.REACT_APP_AWS_REGION;
+const accessKeyId = process.env.REACT_APP_IAM_ACCESS_KEY_ID_DEV;
+const secretAccessKey = process.env.REACT_APP_IAM_SECRET_ACCESS_KEY_DEV;
+
+export const uploadProfileImage = async (image:File, fileName:string) => {
+  const s3 = new AWS.S3({
+    accessKeyId,
+    secretAccessKey,
+    region,
+    apiVersion: 'latest',
+  });
+
+  const uploadPayload = {
+    Bucket: `${bucketName}/profile_image`,
+    Body: image,
+    Key: fileName,
+  }
+
+  try {
+    const response = <S3UploadResponse><unknown>await s3.upload(uploadPayload).promise()
+    return response?.Location
+  } catch (err) {
+    console.error(err);
+  }
+}
