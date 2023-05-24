bb99cb1c2ad61582440e40e3c4a3755087869721
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-05-24 21:46:41 +0100

    fix table error

--- src/components/tables/SuperadminTable.tsx
+++ src/components/tables/SuperadminTable.tsx
@@ -1,50 +1,49 @@
 import {GetHospitalResponseData, HospitalRowData} from "../../types/superadmin";
 import {TbArrowsMoveVertical} from "react-icons/tb";
-// import { ColumnDef } from "@tanstack/react-table"
 
 export const SuperadminHospitalColumn = () => {
   const columnItem = [
 
     {
-      Header: <th colSpan={1} scope="col" className="font-inter text-xs font-medium text-custom-description">
-        <div className="flex items-center gap-3 whitespace-nowrap px-6 py-3">
+      Header: <th colSpan={1} scope="col" className="!font-inter !text-xs !font-medium">
+        <div className="!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3">
           <div
-            className="group flex cursor-pointer select-none flex-row items-center gap-2 font-inter text-sm font-medium text-brand-description-text outline-none">
+            className="!group !flex !cursor-pointer !select-none !flex-row !items-center !gap-2 !font-inter !text-sm !font-medium !outline-none">
             <input
-              className="grid h-6 w-6 flex-shrink-0 place-items-center rounded-md border p-1 outline-none
-                      ring-offset-0 transition-[background-color,_border-color,_box-shadow] focus:ring-2
-                      focus:ring-offset-2 group-hover:ring-2 group-hover:ring-offset-2 border-custom-gray-100 bg-white
-                      ring-custom-primary-400"
+              className="!grid !h-6 !w-6 !flex-shrink-0 !place-items-center !rounded-md border !p-1 !outline-none
+                      !ring-offset-0 !transition-[background-color,_border-color,_box-shadow] !focus:ring-2
+                      !focus:ring-offset-2 !group-hover:ring-2 !group-hover:ring-offset-2 !border-custom-gray-100 !bg-white
+                      !ring-custom-primary-400"
               type="checkbox"
             />
           </div>
         </div>
       </th>,
-      accessor: 'checkbox', // accessor is the "key" in the data
+      accessor: 'checkbox',
     },
 
     {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Name
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By name`)}/>
         </div>
       </th>,
-      accessor: 'name', // accessor is the "key" in the data
+      accessor: 'name',
     },
 
     {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
-          Email
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
+          Name
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By Email`)}/>
         </div>
       </th>,
@@ -55,9 +54,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Sites
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By Sites`)}/>
         </div>
@@ -69,9 +68,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Phone
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By Phone`)}/>
         </div>
@@ -83,9 +82,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Address
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By Address`)}/>
         </div>
@@ -97,9 +96,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           City
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By City`)}/>
         </div>
@@ -111,9 +110,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           State
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By State`)}/>
         </div>
@@ -125,9 +124,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Country
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By Country`)}/>
         </div>
@@ -139,9 +138,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Joined On
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By created_at`)}/>
         </div>
@@ -153,9 +152,9 @@ export const SuperadminHospitalColumn = () => {
       Header: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none !font-black`}>
           Action
           <TbArrowsMoveVertical size={15} onClick={() => console.log(`Sort By Action`)}/>
         </div>
@@ -172,15 +171,15 @@ export const SuperadminHospitalRow = (data:GetHospitalResponseData[]) => {
 
   data?.map((item:GetHospitalResponseData, idx:number) => {
     rowItems.push({
-      checkbox: <th colSpan={1} scope="col" className="font-inter text-xs font-medium text-custom-description">
-        <div className="flex items-center gap-3 whitespace-nowrap px-6 py-3">
+      checkbox: <th colSpan={1} scope="col" className="f!ont-inter !text-xs !font-medium">
+        <div className="!flex items-center gap-3 whitespace-nowrap px-6 py-3">
           <div
-            className="group flex cursor-pointer select-none flex-row items-center gap-2 font-inter text-sm font-medium text-brand-description-text outline-none">
+            className="!group !flex !cursor-pointer !select-none !flex-row !items-center !gap-2 !font-inter !text-sm !font-medium !outline-none">
             <input
-              className="grid h-6 w-6 flex-shrink-0 place-items-center rounded-md border p-1 outline-none
-                      ring-offset-0 transition-[background-color,_border-color,_box-shadow] focus:ring-2
-                      focus:ring-offset-2 group-hover:ring-2 group-hover:ring-offset-2 border-custom-gray-100 bg-white
-                      ring-custom-primary-400"
+              className="!grid !h-6 !w-6 !flex-shrink-0 !place-items-center !rounded-md !border !p-1 !outline-none
+                      !ring-offset-0 !transition-[background-color,_border-color,_box-shadow] !focus:ring-2
+                      !focus:ring-offset-2 !group-hover:ring-2 !group-hover:ring-offset-2 !border-custom-gray-100 !bg-white
+                      !ring-custom-primary-400"
               type="checkbox"
             />
           </div>
@@ -189,81 +188,81 @@ export const SuperadminHospitalRow = (data:GetHospitalResponseData[]) => {
       name: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.name}
         </div>
       </th>,
       email: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.email}
         </div>
       </th>,
       site_count: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.site_count}
         </div>
       </th>,
       phone: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.phone}
         </div>
       </th>,
       address: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.address}
         </div>
       </th>,
       city: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.city}
         </div>
       </th>,
       state: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.state}
         </div>
       </th>,
       country: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.country}
         </div>
       </th>,
       created_at: <th
         colSpan={1}
         scope="col"
-        className="font-inter text-xs font-medium text-custom-description"
+        className="!font-inter !text-xs !font-medium !text-custom-description"
       >
-        <div className={`flex items-center gap-3 whitespace-nowrap px-6 py-3 cursor-pointer select-none`}>
+        <div className={`!flex !items-center !gap-3 !whitespace-nowrap !px-6 !py-3 !cursor-pointer !select-none`}>
           {item?.created_at}
         </div>
       </th>,
