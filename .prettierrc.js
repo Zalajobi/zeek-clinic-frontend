cb129a1d7b074de514d0164ae9dcc49b73d3f188
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-06-19 14:58:21 +0100

    update prettier rpc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   semi: true,
   tabWidth: 2,
   singleQuote: true,
   bracketSpacing: true,
   bracketSameLine: true,
   printWidth: 80,
+  pluginSearchDirs: false,
+  singleAttributePerLine: true,
 };
