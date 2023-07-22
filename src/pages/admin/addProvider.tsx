e5b817b5123f94940681a429e84390cc07f914bb
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-07-23 00:28:50 +0100

    new style setup for create new provider

--- src/pages/admin/addProvider.tsx
+++ src/pages/admin/addProvider.tsx
@@ -66,7 +66,7 @@ export const AddProvider = () => {
           <div
             className={`w-full h-full p-6 grid grid-cols-1 gap-6 grid-cols-[30%_70%]`}>
             <div
-              className={`w-full h-full p-4 rounded-2xl shadow-2xl shadow-[#52525b]`}>
+              className={`w-full h-full p-4 rounded-2xl shadow-2xl shadow-[#52525b] max-h-[400px]`}>
               <ImageUpload
                 bucketFolder={`/hospital_image`}
                 url={profilePic}
@@ -76,7 +76,8 @@ export const AddProvider = () => {
 
             <div
               className={`w-full h-full p-4 rounded-2xl shadow-2xl shadow-[#52525b]`}>
-              <div className={`w-full grid gap-6 grid-cols-1 lg:grid-cols-5`}>
+              <div
+                className={`w-full grid gap-6 grid-cols-1 mb-2 lg:grid-cols-5`}>
                 <SelectInput
                   label={`Title`}
                   options={providersTitleSelectInput}
@@ -119,7 +120,8 @@ export const AddProvider = () => {
                 />
               </div>
 
-              <div className={`w-full grid gap-6 grid-cols-1 lg:grid-cols-2`}>
+              <div
+                className={`w-full grid gap-6 grid-cols-1 my-2 lg:grid-cols-2`}>
                 <TextInput
                   label={`Middle Name`}
                   className={`my-3`}
@@ -143,251 +145,263 @@ export const AddProvider = () => {
                   id={`gender`}
                   errorMsg={errors.gender?.message ?? ''}
                 />
+
+                <DateInput
+                  label={`Date Or Birth`}
+                  placeholder={`DD/MM/YYYY`}
+                  className={`my-3`}
+                  errorMsg={errors.dob?.message ?? ''}
+                  id={`dob`}
+                  register={register}
+                  icon={
+                    <FaCalendarAlt
+                      size={20}
+                      className={`${errors.dob?.message ? 'text-red-500' : ''}`}
+                    />
+                  }
+                />
+
+                <TextInput
+                  label={`Email`}
+                  placeholder={`jane@doe.com`}
+                  className={`my-3 w-full`}
+                  errorMsg={errors.email?.message ?? ''}
+                  id={`email`}
+                  register={register}
+                  type={`email`}
+                  icon={
+                    <MdEmail
+                      size={20}
+                      className={`${
+                        errors.email?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
+
+                <TextInput
+                  label={`Password`}
+                  placeholder={`********`}
+                  className={`my-3 w-full`}
+                  errorMsg={errors.password?.message ?? ''}
+                  id={`password`}
+                  register={register}
+                  type={`password`}
+                  icon={
+                    <FaEye
+                      size={20}
+                      className={`${
+                        errors.password?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
+
+                <TextInput
+                  label={`Staff ID`}
+                  placeholder={`NA92NS90D2KNS`}
+                  className={`my-3 w-full`}
+                  errorMsg={errors.staff_id?.message ?? ''}
+                  id={`staff_id`}
+                  register={register}
+                  type={`text`}
+                  icon={
+                    <HiIdentification
+                      size={20}
+                      className={`${
+                        errors.staff_id?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
               </div>
-            </div>
-          </div>
 
-          <div
-            className={`grid grid-col-1 w-full h-full shadow-2xl p-6 my-6 gap-6 lg:grid-cols-4`}>
-            <div
-              className={`w-full grid grid-cols-1 gap-4 items-center lg:grid-cols-4 lg:col-span-3`}>
-              <DateInput
-                label={`Date Or Birth`}
-                placeholder={`DD/MM/YYYY`}
-                className={`my-3 lg:col-span-2`}
-                errorMsg={errors.dob?.message ?? ''}
-                id={`dob`}
-                register={register}
-                icon={
-                  <FaCalendarAlt
-                    size={20}
-                    className={`${errors.dob?.message ? 'text-red-500' : ''}`}
-                  />
-                }
-              />
-            </div>
+              <div
+                className={`w-full grid gap-6 grid-cols-1 my-2 lg:grid-cols-3`}>
+                <TextInput
+                  label={`Username`}
+                  placeholder={`john_doe`}
+                  className={`my-3 w-full`}
+                  errorMsg={errors.username?.message ?? ''}
+                  id={`username`}
+                  register={register}
+                  type={`text`}
+                  icon={
+                    <FaUser
+                      size={20}
+                      className={`${
+                        errors.username?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
 
-            <div
-              className={`grid grid-cols-1 lg:grid-cols-4 gap-6 lg:col-span-6`}>
-              <TextInput
-                label={`Email`}
-                placeholder={`jane@doe.com`}
-                className={`my-3 w-full`}
-                errorMsg={errors.email?.message ?? ''}
-                id={`email`}
-                register={register}
-                type={`email`}
-                icon={
-                  <MdEmail
-                    size={20}
-                    className={`${errors.email?.message ? 'text-red-500' : ''}`}
-                  />
-                }
-              />
+                <SelectInput
+                  label={`Relationship Status`}
+                  options={relationshipStatus}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'relationship_status'}
+                  errorMsg={errors.relationship_status?.message ?? ''}
+                  enableFilter={true}
+                />
 
-              <TextInput
-                label={`Password`}
-                placeholder={`********`}
-                className={`my-3 w-full`}
-                errorMsg={errors.password?.message ?? ''}
-                id={`password`}
-                register={register}
-                type={`password`}
-                icon={
-                  <FaEye
-                    size={20}
-                    className={`${
-                      errors.password?.message ? 'text-red-500' : ''
-                    }`}
-                  />
-                }
-              />
+                <SelectInput
+                  label={`Religion`}
+                  options={religions.sort((a, b) =>
+                    a.placeholder.localeCompare(b.placeholder)
+                  )}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'religion'}
+                  errorMsg={errors.religion?.message ?? ''}
+                  enableFilter={true}
+                />
+              </div>
 
-              <TextInput
-                label={`Staff ID`}
-                placeholder={`NA92NS90D2KNS`}
-                className={`my-3 w-full`}
-                errorMsg={errors.staff_id?.message ?? ''}
-                id={`staff_id`}
-                register={register}
-                type={`text`}
-                icon={
-                  <HiIdentification
-                    size={20}
-                    className={`${
-                      errors.staff_id?.message ? 'text-red-500' : ''
-                    }`}
-                  />
-                }
-              />
+              <div
+                className={`w-full grid gap-6 grid-cols-1 my-2 lg:grid-cols-2`}>
+                <SelectInput
+                  label={`Department`}
+                  options={departments}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'department'}
+                  errorMsg={errors.department?.message ?? ''}
+                  enableFilter={true}
+                />
 
-              <TextInput
-                label={`Username`}
-                placeholder={`john_doe`}
-                className={`my-3 w-full`}
-                errorMsg={errors.username?.message ?? ''}
-                id={`username`}
-                register={register}
-                type={`text`}
-                icon={
-                  <FaUser
-                    size={20}
-                    className={`${
-                      errors.username?.message ? 'text-red-500' : ''
-                    }`}
-                  />
-                }
-              />
+                <SelectInput
+                  label={`Role`}
+                  options={roles}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'role'}
+                  errorMsg={errors.role?.message ?? ''}
+                  enableFilter={true}
+                />
 
-              <SelectInput
-                label={`Relationship Status`}
-                options={relationshipStatus}
-                className={`w-full my-3`}
-                register={register}
-                id={'relationship_status'}
-                errorMsg={errors.relationship_status?.message ?? ''}
-                enableFilter={true}
-              />
+                <SelectInput
+                  label={`Service Area`}
+                  options={serviceArea}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'serviceArea'}
+                  errorMsg={errors.serviceArea?.message ?? ''}
+                  enableFilter={true}
+                />
 
-              <SelectInput
-                label={`Religion`}
-                options={religions.sort((a, b) =>
-                  a.placeholder.localeCompare(b.placeholder)
-                )}
-                className={`w-full my-3`}
-                register={register}
-                id={'religion'}
-                errorMsg={errors.religion?.message ?? ''}
-                enableFilter={true}
-              />
+                <SelectInput
+                  label={`Unit`}
+                  options={units}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'unit'}
+                  errorMsg={errors.unit?.message ?? ''}
+                  enableFilter={true}
+                />
+              </div>
 
-              <SelectInput
-                label={`Department`}
-                options={departments}
-                className={`w-full my-3`}
-                register={register}
-                id={'department'}
-                errorMsg={errors.department?.message ?? ''}
-                enableFilter={true}
-              />
+              <div
+                className={`w-full grid gap-6 grid-cols-1 my-2 lg:grid-cols-3`}>
+                <SelectInput
+                  label={`Country`}
+                  options={allCountries}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'country'}
+                  errorMsg={errors.country?.message ?? ''}
+                  enableFilter={true}
+                  change={(e) => onUpdateCountry(e.target.value)}
+                />
 
-              <SelectInput
-                label={`Role`}
-                options={roles}
-                className={`w-full my-3`}
-                register={register}
-                id={'role'}
-                errorMsg={errors.role?.message ?? ''}
-                enableFilter={true}
-              />
+                <SelectInput
+                  label={`State`}
+                  options={allCountryStates}
+                  className={`w-full my-3`}
+                  register={register}
+                  id={'state'}
+                  errorMsg={errors.state?.message ?? ''}
+                  enableFilter={true}
+                />
 
-              <SelectInput
-                label={`Service Area`}
-                options={serviceArea}
-                className={`w-full my-3`}
-                register={register}
-                id={'serviceArea'}
-                errorMsg={errors.serviceArea?.message ?? ''}
-                enableFilter={true}
-              />
+                <TextInput
+                  label={`City`}
+                  className={`my-3 w-full`}
+                  id={`city`}
+                  register={register}
+                  icon={
+                    <FaCity
+                      size={20}
+                      className={`${
+                        errors.city?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
+              </div>
 
-              <SelectInput
-                label={`Unit`}
-                options={units}
-                className={`w-full my-3`}
-                register={register}
-                id={'unit'}
-                errorMsg={errors.unit?.message ?? ''}
-                enableFilter={true}
-              />
+              <div
+                className={`w-full grid gap-6 grid-cols-1 my-2 lg:grid-cols-2`}>
+                <TextInput
+                  label={`Zip Code`}
+                  className={`my-3 w-full`}
+                  errorMsg={errors.zip_code?.message ?? ''}
+                  id={`zip_code`}
+                  register={register}
+                  icon={
+                    <TbZoomInAreaFilled
+                      size={20}
+                      className={`${
+                        errors.zip_code?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
 
-              <SelectInput
-                label={`Country`}
-                options={allCountries}
-                className={`w-full my-3`}
-                register={register}
-                id={'country'}
-                errorMsg={errors.country?.message ?? ''}
-                enableFilter={true}
-                change={(e) => onUpdateCountry(e.target.value)}
-              />
+                <TextInput
+                  label={`Phone Number`}
+                  placeholder={`+2347053980998`}
+                  className={`my-3 w-full`}
+                  errorMsg={errors.phone?.message ?? ''}
+                  id={`phone`}
+                  register={register}
+                  type={`tel`}
+                  prefix={`${phoneCode ? '+' + phoneCode : ''}`}
+                  icon={
+                    <FaPhone
+                      size={20}
+                      className={`${
+                        errors.phone?.message ? 'text-red-500' : ''
+                      }`}
+                    />
+                  }
+                />
+              </div>
 
-              <SelectInput
-                label={`State`}
-                options={allCountryStates}
-                className={`w-full my-3`}
-                register={register}
-                id={'state'}
-                errorMsg={errors.state?.message ?? ''}
-                enableFilter={true}
-              />
+              <div
+                className={`w-full grid gap-6 grid-cols-1 my-2 lg:grid-cols-3`}>
+                <CheckboxInput
+                  label={`Consultant`}
+                  className={`my-3 w-full flex items-center`}
+                  id={`is_consultant`}
+                  register={register}
+                />
 
-              <TextInput
-                label={`City`}
-                className={`my-3 w-full`}
-                id={`city`}
-                register={register}
-                icon={
-                  <FaCity
-                    size={20}
-                    className={`${errors.city?.message ? 'text-red-500' : ''}`}
-                  />
-                }
-              />
+                <CheckboxInput
+                  label={`Specialist`}
+                  className={`my-3 w-full flex items-center`}
+                  id={`is_specialist`}
+                  register={register}
+                />
 
-              <TextInput
-                label={`Zip Code`}
-                className={`my-3 w-full`}
-                errorMsg={errors.zip_code?.message ?? ''}
-                id={`zip_code`}
-                register={register}
-                icon={
-                  <TbZoomInAreaFilled
-                    size={20}
-                    className={`${
-                      errors.zip_code?.message ? 'text-red-500' : ''
-                    }`}
-                  />
-                }
-              />
-
-              <TextInput
-                label={`Phone Number`}
-                placeholder={`+2347053980998`}
-                className={`my-3 w-full`}
-                errorMsg={errors.phone?.message ?? ''}
-                id={`phone`}
-                register={register}
-                type={`tel`}
-                prefix={`${phoneCode ? '+' + phoneCode : ''}`}
-                icon={
-                  <FaPhone
-                    size={20}
-                    className={`${errors.phone?.message ? 'text-red-500' : ''}`}
-                  />
-                }
-              />
-
-              <CheckboxInput
-                label={`Consultant`}
-                className={`my-3 w-full flex items-center`}
-                id={`is_consultant`}
-                register={register}
-              />
-
-              <CheckboxInput
-                label={`Specialist`}
-                className={`my-3 w-full flex items-center`}
-                id={`is_specialist`}
-                register={register}
-              />
-
-              <CheckboxInput
-                label={`Appointments`}
-                className={`my-3 w-full flex items-center`}
-                id={`appointments`}
-                register={register}
-              />
+                <CheckboxInput
+                  label={`Appointments`}
+                  className={`my-3 w-full flex items-center`}
+                  id={`appointments`}
+                  register={register}
+                />
+              </div>
             </div>
           </div>
 
