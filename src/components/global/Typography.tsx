43bf9b67472734dfdf5e6e743225fb318c86626b
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-08-01 19:49:09 +0100

    typography

--- src/components/global/Typography.tsx
+++ src/components/global/Typography.tsx
@@ -1,4 +1,5 @@
-import { Fragment, ReactNode } from 'react';
+import { ReactNode } from 'react';
+import clsx from 'clsx';
 
 interface TypographyProps {
   text: string;
@@ -6,6 +7,7 @@ interface TypographyProps {
   className?: string;
   iconBefore?: ReactNode;
   iconAfter?: ReactNode;
+  size?: 'xl' | 'lg' | 'md' | 'sm' | 'xs';
 }
 
 interface TypographyWithLinkProps {
@@ -22,9 +24,24 @@ export const Typography = ({
   iconBefore,
   iconAfter,
   Tag = 'p',
+  size = 'md',
 }: TypographyProps) => {
+  const classes = clsx(`text-[#0E0F17] font-bold ${className}`, {
+    'text-[34px] leading-[44px]': Tag === 'h1',
+    'text-[26px] leading-[35px]': Tag === 'h2',
+    'text-[22px] leading-[30px]': Tag === 'h3',
+    'text-[18px] leading-[26px]': Tag === 'h4',
+    'text-[16px] leading-[24px]': Tag === 'h5',
+    'text-[14px] leading-[21px]': Tag === 'h6',
+    '!font-normal': Tag === 'p',
+    '!text-[20px]': size === 'xl',
+    '!text-[18px]': size === 'lg',
+    '!text-[16px]': size === 'md',
+    '!text-[14px]': size === 'sm',
+    '!text-[12px]': size === 'xs',
+  });
   return (
-    <Tag className={`text-[#0E0F17] font-extrabold ${className}`}>
+    <Tag className={classes}>
       {iconBefore} {text} {iconAfter}
     </Tag>
   );
@@ -40,7 +57,7 @@ export const TypographyWithLink = ({
   return (
     <a
       href={to}
-      className={`text-[#C4C4C6] text-base font-extrabold hover:cursor-pointer hover:no-underline hover:text-[#C4C4C6] ${className}`}>
+      className={`text-[#C4C4C6] text-[700] text-base not-italic hover:cursor-pointer hover:no-underline hover:text-[#C4C4C6] ${className}`}>
       {iconBefore} {text} {iconAfter}
     </a>
   );
