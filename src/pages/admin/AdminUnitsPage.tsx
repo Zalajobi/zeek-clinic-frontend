687eb953a171024d5e3705e1c662447054158a94
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-10-16 07:41:41 +0100

    create department information

---
+++ src/pages/admin/AdminUnitsPage.tsx
@@ -0,0 +1,11 @@
+import AdminBaseTemplate from '@layout/admin/AdminBaseTemplate';
+
+const AdminUnitsPage = () => {
+  return (
+    <AdminBaseTemplate>
+      <h1>Admin Units Page</h1>
+    </AdminBaseTemplate>
+  );
+};
+
+export default AdminUnitsPage;
