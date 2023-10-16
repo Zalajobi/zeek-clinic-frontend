18c13ac313638135db2b042b86ae47dd928bf133
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-10-16 08:02:52 +0100

    units page admin routes and site description

--- src/hooks/admin/units/useUnitsPage.ts
+++ src/hooks/admin/units/useUnitsPage.ts
@@ -2,9 +2,18 @@ import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import { AccountServiceApiResponse } from '@typeSpec/apiResponses';
 import { axiosGetRequestUserService } from '@lib/axios';
+import { useState } from 'react';
 
 export const useAdminDepartmentsPage = () => {
   const { siteId } = useParams();
+
+  // Create New Unit
+  const [showCreateUnitModal, setShowCreateUnitModal] = useState(false);
+  const [newUnit, setNewUnit] = useState({
+    name: '',
+    description: '',
+    siteId,
+  });
 
   const {
     data: siteData,
@@ -19,12 +28,30 @@ export const useAdminDepartmentsPage = () => {
     }
   );
 
+  // Create New Unit
+  const onUpdateNewUnitDescription = (description: string) =>
+    setNewUnit({ ...newUnit, description });
+
+  const onUpdateNewUnitName = (name: string) =>
+    setNewUnit({ ...newUnit, name });
+  const createNewUnit = () => {
+    console.log({
+      message: createNewUnit,
+      newUnit,
+    });
+  };
+
   return {
     // Values
     siteData,
     siteDataLoading,
     siteDataError,
+    showCreateUnitModal,
 
     // Functions
+    setShowCreateUnitModal,
+    createNewUnit,
+    onUpdateNewUnitDescription,
+    onUpdateNewUnitName,
   };
 };
--- src/pages/admin/AdminUnitsPage.tsx
+++ src/pages/admin/AdminUnitsPage.tsx
@@ -1,9 +1,95 @@
 import AdminBaseTemplate from '@layout/admin/AdminBaseTemplate';
+import { useAdminDepartmentsPage } from '@hooks/admin/units/useUnitsPage';
+import { Typography } from '@components/global/dialog/Typography';
+import { BasicOutlineButton } from '@components/global/CustomButton';
+import { FaPlus } from 'react-icons/fa';
+import { CgExport } from 'react-icons/cg';
+import AdminSiteInfo from '@components/admin/AdminSiteInfo';
+import { AddNewDeptServiceAreaModal } from '@components/modals/quickAction/AddNewDeptServiceAreaModal';
+import AdminRoutes from '@components/admin/AdminRoutes';
 
 const AdminUnitsPage = () => {
+  const adminData = JSON.parse(localStorage.getItem('adminData') as string);
+  const {
+    // Values
+    siteData,
+    siteDataLoading,
+    // siteDataError,
+    showCreateUnitModal,
+
+    // Functions
+    setShowCreateUnitModal,
+    createNewUnit,
+    onUpdateNewUnitDescription,
+    onUpdateNewUnitName,
+  } = useAdminDepartmentsPage();
+
   return (
     <AdminBaseTemplate>
-      <h1>Admin Units Page</h1>
+      <div className={`flex flex-col w-full`}>
+        <div className={`grid grid-cols-[80%_20%] gap-4`}>
+          <Typography
+            text={`Welcome Back, ${adminData?.personalInfo?.first_name ?? ''}`}
+            Tag={`h1`}
+            size={`2xl`}
+            className={`text-left`}
+          />
+
+          <div className={`w-full flex justify-end mb-6`}>
+            <BasicOutlineButton
+              type={`primary`}
+              text={`Create New`}
+              className={`ml-4 min-w-[130px]`}
+              click={() => setShowCreateUnitModal(!showCreateUnitModal)}
+              iconBefore={
+                <FaPlus
+                  size={20}
+                  className={`mr-2`}
+                />
+              }
+            />
+
+            <BasicOutlineButton
+              type={`primary`}
+              text={`Export`}
+              className={`ml-4 min-w-[130px]`}
+              iconBefore={
+                <CgExport
+                  size={20}
+                  className={`mr-2`}
+                />
+              }
+            />
+          </div>
+        </div>
+
+        <AdminSiteInfo
+          dataLoading={siteDataLoading}
+          id={siteData?.data?.id ?? ''}
+          address={siteData?.data?.address ?? ''}
+          name={siteData?.data?.name ?? ''}
+          email={siteData?.data?.email ?? ''}
+          country={siteData?.data?.country ?? ''}
+          phone={siteData?.data?.phone ?? ''}
+          state={siteData?.data?.state ?? ''}
+          city={siteData?.data?.city ?? ''}
+          created_at={siteData?.data?.city ?? ''}
+        />
+
+        <AdminRoutes
+          siteId={siteData?.data?.id ?? ''}
+          id={adminData?.id}
+        />
+      </div>
+
+      <AddNewDeptServiceAreaModal
+        updateDescription={onUpdateNewUnitDescription}
+        updateName={onUpdateNewUnitName}
+        handler={() => setShowCreateUnitModal(!showCreateUnitModal)}
+        name={`Department`}
+        open={showCreateUnitModal}
+        submit={createNewUnit}
+      />
     </AdminBaseTemplate>
   );
 };
