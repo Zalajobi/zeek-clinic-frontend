46edd1237a045869e8df47e4ef9839b56aa16216
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-10-24 16:15:59 +0100

    role page and route specification

---
+++ src/pages/admin/AdminRolesPage.tsx
@@ -0,0 +1,11 @@
+import AdminBaseTemplate from '@layout/admin/AdminBaseTemplate';
+
+const AdminRolesPage = () => {
+  return (
+    <AdminBaseTemplate>
+      <h1>Roles</h1>
+    </AdminBaseTemplate>
+  );
+};
+
+export default AdminRolesPage;
